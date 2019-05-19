e0e3ecaa7e5376ab1c5f7f2ce3a0980bdb248af5
nistoralexandra <nistor_ada@yahoo.com>
2019-05-19 10:28:04 +0300

    added dropdown menu for Proximity sensors

--- public/javascripts/sensors.js
+++ public/javascripts/sensors.js
@@ -1,4 +1,16 @@
 function onclick_logout() {
-    console.log("am dat click pe logot");
+    console.log("am dat click pe logout");
     window.location.replace("/logout");
+}
+
+function onclick_sensor1() {
+    document.getElementById("p-sensor-name").innerHTML = "Proximity - S1"
+}
+
+function onclick_sensor2() {
+    document.getElementById("p-sensor-name").innerHTML = "Proximity - S2"
+}
+
+function onclick_senzor3() {
+    document.getElementById("p-sensor-name").innerHTML = "Proximity - S3"
 }
