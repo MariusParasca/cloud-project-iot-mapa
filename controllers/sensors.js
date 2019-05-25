57ee4f5ee74bb48b1e827a22eb07667f0ca95dda
MariusParasca <mrs.parasca@gmail.com>
2019-05-25 17:49:10 +0300

    added working sensor page

--- controllers/sensors.js
+++ controllers/sensors.js
@@ -1,5 +1,4 @@
 const models = require('../models');
-const PROXIMITY = "proximity";
 
 exports.refreshSensors = function(req, res, next) {
   if (req.session.user) {
@@ -36,18 +35,7 @@ function sendSensorsValues(user) {
 
 function renderSensorPage(user) {
   sensors = JSON.parse(user.dataValues["Sensors"]);
-  console.log(sensors);
-  // var sensors = {
-  //   temperature: valueTemperature, 
-  //   umidity: valueUmidity,
-  //   proximity: {
-  //     sensor1: valueProximity1,
-  //     sensor2: valueProximity2,
-  //     sensor3: valueProximity3
-  //   }
-  // };
-  // res.render('sensors', { title: 'IOT Automation', sensors: sensors });
-  this.res.render('sensors', { title: 'IOT Automation' });
+  this.res.render('sensors', { title: 'IOT Automation', sensors: sensors });
 }
 
 function updateSensorValuesInDB(actualValues, email) {
@@ -62,57 +50,19 @@ function updateSensorValuesInDB(actualValues, email) {
 }
 
 function handleSendDataRequest(user) {
-  var actualValues = this.req.body;
-  var clientValues = JSON.parse(user.dataValues["Sensors"]);
-
-  //if (areSensorValueDifferent(actualValues, clientValues)) {
+  if(user) {
+    var actualValues = this.req.body;
     updateSensorValuesInDB(actualValues, user.dataValues["email"]);
-    //updateSensorValuesInView()
-  //}
+    this.res.status(200).send();
+  } else {
+    this.res.status(400).send();
+  }
 }
 
 exports.sendData = function(req, res, next) {
-  console.log(req.header);
-  if (req.session.user) {
     models.User.findOne({
       where: {
-        email: req.session.user,
         keyid: req.header("Client-Key")
       }
     }).then(handleSendDataRequest.bind({ req: req, res: res }));
-    res.status(200).send();
-  } else {
-    res.status(400).send();
-  }
-}
-
-
-function areSensorValueDifferent(actualValues, clientValues) {
-  if (Object.keys(actualValues).length > Object.keys(clientValues).length)
-    return true;
-  for (let [key, value] of Object.entries(clientValues)) {
-    if (typeof (value) == "obecjt") {
-      if (areSensorValueDifferent(value, actualValues[key]))
-        return true;
-    } else if (value != actualValues[key]) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
-function transformToDbObjectFormat(clientValues) {
-  var proximitySensors = {}
-  var result = {}
-  for (let [key, value] of Object.entries(clientValues)) {
-    if (key.startsWith("sensor")) {
-      proximitySensors[key] = value;
-    } else {
-      result[key] = value;
-    }
-  }
-
-  result[PROXIMITY] = proximitySensors;
-  return result;
 }
