7e1db0db63fa6e464a532a3e295e383f677e5f9b
nistoralexandra <nistor_ada@yahoo.com>
2019-05-26 11:14:13 +0300

    modified Proximity display format

--- public/javascripts/sensors.js
+++ public/javascripts/sensors.js
@@ -1,17 +1,17 @@
-function onclick_logout() {
-    console.log("am dat click pe logout");
-    window.location.replace("/logout");
+function onclick_sensor(number, sensorValue) {
+    document.getElementById('prox-sensor-name').innerHTML = 'Proximity - S' + number;
+    var value = sensorValue == 1 ? 'True' : 'False'
+    document.getElementById('prox').innerHTML= value
 }
 
 function onclick_modal() {
     document.querySelector('.modal-bg').style.display = 'flex';
-}
 
 function onclick_modal_close() {
     document.querySelector('.modal-bg').style.display = 'none';
 }
 
-function onclick_sensor(number, sensor) {
-    document.getElementById('prox-sensor-name').innerHTML = 'Proximity - S' + number;
-    document.getElementById('prox').innerHTML = sensor;
+function onclick_logout() {
+    console.log("am dat click pe logout");
+    window.location.replace("/logout");
 }
